7bc29322f13ffd4f0be34d209d259607d5274ece
Koumudi <pangulurikoumudi@gmail.com>
2019-04-22 16:58:17 +0530

    initial commit

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,24 +6,58 @@ const $messageLocation=document.querySelector('#send-location');
 const $messages=document.querySelector("#messages");
 const $messageInput=document.querySelector("#message-input").innerHTML;
 const locationMessageTemplate=document.querySelector("#Location-message-template").innerHTML;
+const sidebarTemplate=document.querySelector("#sidebar-template").innerHTML;
+
+const {username, room }=Qs.parse(location.search,{ignoreQueryPrefix:true}) //for taking the values from url and parsing it (ignorequeryprefix is to eliminate '?')
+
+
+const autoscroll=()=>{
+  const $newMessage=$messages.lastElementChild
+  const newMessageStyles=getComputedStyle($newMessage)
+  const newMessageMargin=parseInt(newMessageStyles.marginBottom)
+  const newMessageHeight=$newMessage.offsetHeight + newMessageMargin
+  const visibleHeight=$messages.offsetHeight
+  const containerHeight=$messages.scrollHeight
+  const scrollOffset=$messages.scrollTop + visibleHeight
+  if(containerHeight - newMessageHeight <= scrollOffset){
+    $messages.scrollTop= $messages.scrollHeight
+  }
+}
+
+
+
 
 socket.on('message',(message)=>{
 
   console.log(message);
   const html = Mustache.render($messageInput,{
+    username:message.username,
     message:message.text, //message is an object of generateMessage
     createdAt:moment(message.createdAt).format('h:mm a')
   })
   $messages.insertAdjacentHTML('beforeend',html)
+  autoscroll()
 })
 socket.on('LocationMessage',(message)=>{
-  console.log(message)
+  console.log(message )
   const html=Mustache.render(locationMessageTemplate,{
+    username:message.username,
     url:message.url,
     createdAt:moment(message.createdAt).format('h:mm a')
   })
   $messages.insertAdjacentHTML('beforeend',html)
+  autoscroll()
 })
+
+socket.on("roomData",({room,users})=>{
+const html=Mustache.render(sidebarTemplate,{
+  room,
+  users
+})
+document.querySelector("#sidebar").innerHTML=html
+})
+
+ 
 $messageForm.addEventListener('submit',(e)=>{
   e.preventDefault()
   $messageFormButton.setAttribute('disabled','disabled'); //not sending server more than once until it is emitted
@@ -58,4 +92,9 @@ $messageLocation.addEventListener('click',()=>{
     })
   })
 })
-
+socket.emit('join',{username,room},(error)=>{
+  if(error){
+    alert(error)
+    location.href='/'
+  }
+})
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const Filter=require('bad-words');
 const app=express();
 
 const { generateMessage,generateLocationMessage }=require('./utils/messages')
+const{addUser,removeUser,getUser,getUsersInRoom}=require('./utils/users')
+
 const publicDirectory=path.join(__dirname,"../public");
 app.use(express.static(publicDirectory));
 
@@ -16,27 +18,60 @@ io.on('connection',(socket)=>{
   console.log("New Connection established");
   
 
-  socket.emit("message",generateMessage("welcome!"))
-  socket.broadcast.emit("message",generateMessage("new user joined!")); //expect that user, displays it to all users.
-  socket.on("sendmessage",(message,callback)=>{
-    const filter=new Filter();
-    if(filter.isProfane(message)){
-      return callback("profinity");
+ 
+  socket.on('join',(options,callback)=>{
+    const {error,user}=addUser({id:socket.id,...options})
+    if(error){
+      return callback(error)
     }
-    io.emit('message',generateMessage(message));//send data to all clients..
+    socket.join(user.room) //joins the users of particular room
+    socket.emit("message",generateMessage('Admin',"welcome!"))
+    socket.broadcast.to(user.room).emit("message",generateMessage('Admin',`${user.username} has joined!`));
+    //socket.broadcast.to(room).emit is used to send info to that room
+
+    //getting users in a particular room
+    io.to(user.room).emit("roomData",{
+      room: user.room,
+      users:getUsersInRoom(user.room)
+    })
+
     callback()
+  })
+   //expect that user, displays it to all users.
+  socket.on("sendmessage",(message,callback)=>{
+    const user=getUser(socket.id)
+    
+      const filter=new Filter();
+      if(filter.isProfane(message)){
+        return callback("profinity");
+      }
+      io.to(user.room).emit('message',generateMessage(user.username,message));//send data to all clients..
+      callback()
+    
+    
   })
 
   socket.on('sendLocation',(coords,callback)=>{
-    io.emit('LocationMessage',generateLocationMessage(`https://google.com/maps?${coords.latitude},${coords.longitude}`))
+    const user=getUser(socket.id)
+    io.to(user.room).emit('LocationMessage',generateLocationMessage(user.username,`https://google.com/maps?${coords.latitude},${coords.longitude}`))
     callback()
   })
-
 
 
 
   socket.on("disconnect",()=>{
-    io.emit('message',generateMessage("user left!"));
+    const user=removeUser(socket.id)
+    if(user){
+
+      io.to(user.room).emit('message',generateMessage('Admin',`${user.username} left!`));
+      
+      //getting users in a particular room
+
+      io.to(user.room).emit("roomData",{
+        room:user.room,
+        users:getUsersInRoom(user.room)
+      })
+    }
   })
 })
 
